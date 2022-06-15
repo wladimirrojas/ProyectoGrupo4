6dbead5a89802ecabafe29663adf41ec432ce52a
Esancal <esteban.ca2000@outlook.cl>
2022-06-15 10:28:37 -0400

    Not nulls removidos

--- DDBB/marikeiks.sql
+++ DDBB/marikeiks.sql
@@ -13,68 +13,68 @@ DROP TABLE IF EXISTS ventas_productos;
 
 CREATE TABLE sabores (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-descripcion VARCHAR(100) NOT NULL,
-disponible BOOLEAN NOT NULL,
-stock INTEGER NOT NULL);
+nombre VARCHAR(20),
+descripcion VARCHAR(100),
+disponible BOOLEAN,
+stock INTEGER);
 
 CREATE TABLE tipo_productos (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-porcion_cantidad INTEGER NOT NULL,
-sabores_id INTEGER NOT NULL);
+nombre VARCHAR(20),
+porcion_cantidad INTEGER,
+sabores_id INTEGER);
 
 CREATE TABLE decoraciones (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-descripcion VARCHAR(100) NOT NULL,
-stock INTEGER NOT NULL,
-precio INTEGER NOT NULL);
+nombre VARCHAR(20),
+descripcion VARCHAR(100),
+stock INTEGER,
+precio INTEGER);
 
 CREATE TABLE productos (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-descripcion VARCHAR(100) NOT NULL,
-stock INTEGER NOT NULL,
-precio INTEGER NOT NULL,
-vegano BOOLEAN NOT NULL,
-tipo_productos_id INTEGER NOT NULL,
-decoraciones_id INTEGER NOT NULL);
+descripcion VARCHAR(100),
+stock INTEGER,
+precio INTEGER,
+vegano BOOLEAN,
+tipo_productos_id INTEGER,
+decoraciones_id INTEGER);
 
 CREATE TABLE clientes (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre VARCHAR(50) NOT NULL,
-apellido VARCHAR(50) NOT NULL,
-telefono VARCHAR(12) NOT NULL,
-correo VARCHAR(50) NOT NULL,
-direccion VARCHAR(50) NOT NULL);
+nombre VARCHAR(50),
+apellido VARCHAR(50),
+telefono VARCHAR(12),
+correo VARCHAR(50),
+direccion VARCHAR(50));
 
 CREATE TABLE despacho_retiros (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-estado VARCHAR(20) NOT NULL);
+estado VARCHAR(20));
 
 CREATE TABLE ventas (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-fecha_emision DATE NOT NULL,
-fecha_entrega DATE NOT NULL,
-subtotal INTEGER NOT NULL,
-comentario VARCHAR(100) NOT NULL,
-iva_subtotal INTEGER NOT NULL,
-cliente_id INTEGER NOT NULL,
-estado_ventas_id INTEGER NOT NULL,
-despacho_retiros_id INTEGER NOT NULL);
+fecha_emision DATE,
+fecha_entrega DATE,
+subtotal INTEGER,
+comentario VARCHAR(100),
+iva_subtotal INTEGER,
+cliente_id INTEGER,
+estado_ventas_id INTEGER,
+despacho_retiros_id INTEGER);
 
 CREATE TABLE estado_ventas (
 id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre VARCHAR(20) NOT NULL,
-comentario VARCHAR(100) NOT NULL);
+nombre VARCHAR(20),
+comentario VARCHAR(100));
 
 CREATE TABLE ventas_productos (
-productos_id INTEGER NOT NULL,
-ventas_id INTEGER NOT NULL,
-cantidad INTEGER NOT NULL,
-precio_total INTEGER NOT NULL,
-descuento INTEGER NOT NULL,
-iva_total INTEGER NOT NULL,
+productos_id INTEGER,
+ventas_id INTEGER,
+cantidad INTEGER,
+precio_total INTEGER,
+descuento INTEGER,
+iva_total INTEGER,
 PRIMARY KEY (productos_id,ventas_id));
 
 ALTER TABLE tipo_productos ADD CONSTRAINT tipo_productos_sabores_id_sabores_id FOREIGN KEY (sabores_id) REFERENCES sabores(id);
