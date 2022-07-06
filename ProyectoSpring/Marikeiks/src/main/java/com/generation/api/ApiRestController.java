66bbcd3ed3b479abca6208c4823295b74fd355a5
Katherina98 <katherinaorellana.s@gmail.com>
2022-07-06 17:54:30 -0400

    api cliente y estado venta

--- ProyectoSpring/Marikeiks/src/main/java/com/generation/api/ApiRestController.java
+++ ProyectoSpring/Marikeiks/src/main/java/com/generation/api/ApiRestController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.models.Cliente;
+import com.generation.models.EstadoVenta;
 import com.generation.models.Producto;
 import com.generation.models.Sabor;
+import com.generation.services.ClienteService;
+import com.generation.services.EstadoVentaService;
 import com.generation.services.ProductoService;
 import com.generation.services.SaborService;
 
@@ -20,6 +24,12 @@ public class ApiRestController {
 
 	@Autowired
 	SaborService saborService;
+	
+	@Autowired
+	ClienteService clienteService;
+	
+	@Autowired
+	EstadoVentaService estadoVentaService;
 
 	@RequestMapping("/productos")
 	public List<Producto> obtenerProductos() {
@@ -32,4 +42,17 @@ public class ApiRestController {
 
 		return saborService.findAll();
 	}
+	
+	@RequestMapping("/clientes")
+	public List<Cliente> obtenerClientes () {
+	List<Cliente> listaClientes = clienteService.findAll();
+	return listaClientes; 
+	}
+	
+	@RequestMapping("/estadoVenta")
+	public List<EstadoVenta> obtenerEstadoVenta () {
+	List<EstadoVenta> listaEstadosVentas = estadoVentaService.findAll();
+	return listaEstadosVentas; 
+	}
+	
 }
