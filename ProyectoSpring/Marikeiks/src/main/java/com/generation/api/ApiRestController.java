0242e7b3bec0448ba3dcacc741f49ed9d5b4cd89
CataMtnz <catalinamartinezf3@gmail.com>
2022-07-06 18:48:16 -0400

    agregando

--- ProyectoSpring/Marikeiks/src/main/java/com/generation/api/ApiRestController.java
+++ ProyectoSpring/Marikeiks/src/main/java/com/generation/api/ApiRestController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.models.Cliente;
+import com.generation.models.DespachoRetiro;
 import com.generation.models.Producto;
 import com.generation.models.Sabor;
 import com.generation.models.Venta;
+import com.generation.services.ClienteService;
 import com.generation.services.ProductoService;
 import com.generation.services.SaborService;
 
@@ -34,4 +37,25 @@ public class ApiRestController {
 		return saborService.findAll();
 
 	}
+	//Kathy
+	@RequestMapping("/estadoVenta")
+	public List<EstadoVenta> obtenerEstadoVenta () {
+	List<EstadoVenta> listaEstadosVentas = estadoVentaService.findAll();
+	return listaEstadosVentas;
+	}
+
+	@RequestMapping("/clientes")
+	public List<Cliente> obtenerClientes () {
+	List<Cliente> listaClientes = clienteService.findAll();
+	return listaClientes;
+	}
+	//Apis a revisar
+
+	@RequestMapping("/despachosRetiros")
+	public List<DespachoRetiro> obtenerDespachoRetiro(){
+		return DespachoRetiroService.findAll();
+	}
+
+
+	
 }
