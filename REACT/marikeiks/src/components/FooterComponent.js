70e3bdaceec1c837cdc8fe5be781c02cbab0d057
Esancal <esteban.ca2000@outlook.cl>
2022-07-20 11:09:02 -0400

    Style

--- REACT/marikeiks/src/components/FooterComponent.js
+++ REACT/marikeiks/src/components/FooterComponent.js
@@ -1,19 +1,19 @@
-import React from "react";
-import "../styles/style.css"
+import React from 'react';
+import '../styles/styleFooter.css'
 
 const FooterComponent = () => {
   return (
     <footer
-      class="text-center text-lg-start text-white"
+      className='text-center text-lg-start text-white'
       style={{
-        backgroundColor: "#929fba",
+        backgroundColor: '#929fba',
       }}
     >
-      <div class="container p-4 pb-0">
-        <section class="">
-          <div class="row">
-            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4">Marikeiks</h6>
+      <div className='container p-4 pb-0'>
+        <section className=''>
+          <div className='row'>
+            <div className='col-md-3 col-lg-3 col-xl-3 mx-auto mt-3'>
+              <h6 className='text-uppercase mb-4'>Marikeiks</h6>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Delectus ex, eos excepturi voluptatem fugiat illum nulla
@@ -21,63 +21,63 @@ const FooterComponent = () => {
               </p>
             </div>
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className='w-100 clearfix d-md-none' />
 
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4">Productos</h6>
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mt-3'>
+              <h6 className='text-uppercase mb-4'>Productos</h6>
 
               <p>
-                <a class="text-white noLink" href="www.google.cl">
+                <a className='text-white noLink' href='www.google.cl'>
                   Tortas
                 </a>
               </p>
               <p>
-                <a class="text-white noLink" href="www.google.cl">
+                <a className='text-white noLink' href='www.google.cl'>
                   CandiBar
                 </a>
               </p>
             </div>
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className='w-100 clearfix d-md-none' />
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className='w-100 clearfix d-md-none' />
 
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4">Contacto</h6>
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mt-3'>
+              <h6 className='text-uppercase mb-4'>Contacto</h6>
               <p>
-                <i class="mr-3"></i> Direccion (nosecual)
+                <i className='mr-3'></i> Direccion (nosecual)
               </p>
               <p>
-                <i class="mr-3"></i> correo@nose.cl
+                <i className='mr-3'></i> correo@nose.cl
               </p>
               <p>
-                <i class="mr-3"></i> numerotelefono
+                <i className='mr-3'></i> numerotelefono
               </p>
             </div>
 
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4">Siguenos</h6>
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mt-3'>
+              <h6 className='text-uppercase mb-4'>Siguenos</h6>
 
               <p>
                 <a
-                  class="noLink"
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://es-la.facebook.com/MariKeiks/"
+                  className='noLink'
+                  target='_blank'
+                  rel='noreferrer'
+                  href='https://es-la.facebook.com/MariKeiks/'
                 >
-                  <img src="/assets/svg/facebook.svg" alt=""></img>
+                  <img src='/assets/svg/facebook.svg' alt=''></img>
                   Facebook
                 </a>
               </p>
 
               <p>
                 <a
-                  class="noLink"
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.instagram.com/marikeiks/"
+                  className='noLink'
+                  target='_blank'
+                  rel='noreferrer'
+                  href='https://www.instagram.com/marikeiks/'
                 >
-                  <img src="/assets/svg/instagram.svg" alt=""></img>
+                  <img src='/assets/svg/instagram.svg' alt=''></img>
                   Instagram
                 </a>
               </p>
@@ -87,13 +87,13 @@ const FooterComponent = () => {
       </div>
 
       <div
-        class="text-center p-3"
+        className='text-center p-3'
         style={{
-          backgroundColor: "rgba(0, 0, 0, 0.3)",
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
         }}
       >
         <span>© 2022 Copyright: </span>
-        <a class="text-white noLink" href="www.google.cl" rel="noreferrer"> 
+        <a className='text-white noLink' href='www.google.cl' rel='noreferrer'> 
           Marikeiks.cl
         </a>
       </div>
