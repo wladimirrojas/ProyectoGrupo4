b20f8bac85d473f0b867825e1d1acc506cb2beb4
Esancal <esteban.ca2000@outlook.cl>
2022-07-20 12:36:41 -0400

    .

---
+++ REACT/marikeiks/src/components/PortadaComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/portadaStyle.css";
+
+const FooterComponent = () => {
+  <div id="portada">
+    <img className="img-fluid" src="/assets/img/portada.jpeg" alt=""></img>
+    <div id="titulo">
+      <p id="tituloLetra">
+        Marikeiks
+      </p>
+    </div>
+    <div id="BotonContenedor">
+      <button id="Boton" className="btn btn-outline-dark">
+        Comprar Online
+      </button>
+    </div>
+  </div>;
+};
