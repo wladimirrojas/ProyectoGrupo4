9a2b9c835566ebd53e435abfd1e587f4801ae2e5
Katherina98 <katherinaorellana.s@gmail.com>
2022-07-21 17:52:41 -0400

    lista de productos y rutas

--- REACT/marikeiks/src/App.js
+++ REACT/marikeiks/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Inicio from './components/Inicio';
+import ProductoListado from './components/ProductoListado';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
 
 
 const App = () => {
   return (
-      <Inicio/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Inicio/>}></Route>
+      <Route path='/productos' element={<ProductoListado/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 };
-
-
 
 export default App;
--- REACT/marikeiks/src/components/Historia.js
+++ REACT/marikeiks/src/components/Historia.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useCallback} from 'react';
 import TituloHistoria from './TituloHistoria';
 import {CuerpoHistoria, ImagenesHistoria} from './CuerpoHistoria';
 import SloganHistoria from './SloganHistoria';
+import {useNavigate} from 'react-router-dom';
 
 const Historia = () => {
-    return (
+    
+        const navigate = useNavigate();
+        const handleOnClick = useCallback(()=> navigate('/productos', {replace:true}, [navigate]))
+
+        return (
         <div className="contenedorHistoria">
             <div className="row">
             <TituloHistoria/>
@@ -15,6 +20,7 @@ const Historia = () => {
             </div>
             </div>
             <SloganHistoria/>
+            <button type='button' className='btn btn-outline-primary me-2' onClick={handleOnClick}>Ir a tortas</button>
         </div>    
 
     );  
