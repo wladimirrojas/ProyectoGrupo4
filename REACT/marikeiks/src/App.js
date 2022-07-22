d4a0e9f52d8fbacf1f1d505be3add412a987c079
Katherina98 <katherinaorellana.s@gmail.com>
2022-07-22 13:39:33 -0400

    actualizacion app

--- REACT/marikeiks/src/App.js
+++ REACT/marikeiks/src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-// import Inicio from './components/Inicio';
-// import ProductoListado from './components/ProductoListado';
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-=======
 import Inicio from './components/Inicio';
 import ProductoListado from './components/ProductoListado';
 import Faq from './components/Faq';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
->>>>>>> main
-
-import CarritoUsuarioComponent from './testing/CarritoUsuarioComponent';
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    // <BrowserRouter>
-    // <Routes>
-    //   <Route path='/' element={<Inicio/>}></Route>
-    //   <Route path='/productos' element={<ProductoListado/>}></Route>
-    // </Routes>
-    // </BrowserRouter>
-    <CarritoUsuarioComponent/>
-=======
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Inicio/>}></Route>
@@ -30,7 +13,6 @@ const App = () => {
       <Route path='/faq' element={<Faq/>}></Route>
     </Routes>
     </BrowserRouter>
->>>>>>> main
   );
 };
 
