3653602b50badde01e77ab2c7fca39e2baadec9d
Esancal <esteban.ca2000@outlook.cl>
2022-07-25 11:41:52 -0400

    añadido enlace

--- REACT/marikeiks/src/components/PortadaComponent.js
+++ REACT/marikeiks/src/components/PortadaComponent.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useNavigate } from 'react-router-dom';
 import "../styles/portadaStyle.css";
 
 const PortadaComponent = () => {
+
+  const navigate = useNavigate();
+  const handleOnClick = useCallback(() => navigate('/productos', { replace: false }, [navigate]))
+
   return(
   <div id="portada">
     <img
@@ -13,7 +18,7 @@ const PortadaComponent = () => {
       <p id="tituloLetra">Marikeiks</p>
     </div>
     <div id="BotonContenedor">
-      <button id="Boton" className="btn btn-outline-dark">
+      <button id="Boton" className="btn btn-outline-dark" onClick={handleOnClick}>
         Comprar Online
       </button>
     </div>
