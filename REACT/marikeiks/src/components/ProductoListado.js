cee9a67474a3f37c93b6f80a41afa575e80ada47
Esancal <esteban.ca2000@outlook.cl>
2022-07-27 11:06:06 -0400

    indentado

--- REACT/marikeiks/src/components/ProductoListado.js
+++ REACT/marikeiks/src/components/ProductoListado.js
@@ -1,58 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import TarjetaTorta from './TarjetaTorta';
-import getAll from '../services/ProductoService';
-import TituloProducto from './TituloProducto';
-import DireccionamientoFAQ from './DireccionamientaFAQ';
-import '../styles/ProductoTorta.css'
-
-
+import React, { useState, useEffect } from "react";
+import TarjetaTorta from "./TarjetaTorta";
+import getAll from "../services/ProductoService";
+import TituloProducto from "./TituloProducto";
+import DireccionamientoFAQ from "./DireccionamientaFAQ";
+import "../styles/ProductoTorta.css";
 
 const initialProductos = [
-    {
-        id: 0,
-        nombre: '',
-        descripcion: '',
-        stock: 0,
-        precio: 0,
-        vegano: 0,
-        tipoProducto: 0,
-        decoracion: 0, 
-        sabor: 0,
-        cantidad: 0
-
-    }
-]
+  {
+    id: 0,
+    nombre: "",
+    descripcion: "",
+    stock: 0,
+    precio: 0,
+    vegano: 0,
+    tipoProducto: 0,
+    decoracion: 0,
+    sabor: 0,
+    cantidad: 0,
+  },
+];
 
 const ProductoListado = () => {
+  const [productos, setProductos] = useState(initialProductos);
 
-    const [productos, setProductos] = useState(initialProductos);
+  const obtenerProductos = async () => {
+    setProductos(await getAll());
+  };
 
-    const obtenerProductos = async () => {
-        setProductos(await getAll());
-    };
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
 
-    useEffect(
-        () => {
-            obtenerProductos()
-        }, [])
-
-
-    return (
-        <div className="contenedor-productos p-0">
-            <TituloProducto />
-            <div className="row row-cols-4 row-cols-lg-4 g-4 g-lg-4">
-                    {
-                        productos.map(producto =>
-                            <TarjetaTorta
-                                id={producto.id}
-                                producto={producto} />)
-                    }
-                </div>
-            <br />
-            <DireccionamientoFAQ />
-        </div>
-    );
+  return (
+    <div className="contenedor-productos p-0">
+      <TituloProducto />
+      <div className="row row-cols-4 row-cols-lg-4 g-4 g-lg-4">
+        {productos.map((producto) => (
+          <TarjetaTorta id={producto.id} producto={producto} />
+        ))}
+      </div>
+      <br />
+      <DireccionamientoFAQ />
+    </div>
+  );
 };
 
 export default ProductoListado;
-
