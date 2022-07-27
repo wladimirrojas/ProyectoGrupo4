088891cf1e1fb4bac05d435e359e29a5c3801f99
Esancal <esteban.ca2000@outlook.cl>
2022-07-27 10:59:26 -0400

    Cambio de valor numerico a miles

--- REACT/marikeiks/src/components/TarjetaTorta.js
+++ REACT/marikeiks/src/components/TarjetaTorta.js
@@ -3,10 +3,10 @@ import '../styles/ProductoTorta.css'
 import { useCallback } from "react";
 import { useNavigate } from 'react-router-dom';
 
+import FormatNumberComponent from "./utils/FormatNumberComponent";
 
 
-
-const TarjetaTorta = ({ producto }) => {
+const TarjetaTorta = ({ producto }, {number}) => {
     const navigate = useNavigate();
         const handleOnClick = useCallback(() => navigate('/productos/editarorden', { replace: true }, [navigate]));
     return (
@@ -15,7 +15,7 @@ const TarjetaTorta = ({ producto }) => {
                 <img id="imagen-torta" src={producto.url} className="card-img-top" alt={producto.id} />
                 <div id="card-model" className="card-body">
                     <h5 className="card-title">{producto.descripcion}</h5>
-                    <p className="card-text">${producto.precio}</p>
+                    <p className="card-text"><FormatNumberComponent number={producto.precio}/></p>
                     <button className="btn btn-sm btn-outline-primary me-2"onClick = {handleOnClick}>Comprar</button>
                 </div>
             </div>
